c3f7803472376193ff2aa69e84f7b3d04a4ab3dd
Hoddoc <149748563+Hoddoc@users.noreply.github.com>
2025-06-20 11:33:13 +0900

    Add files via upload

--- netlify/functions/dropbox-callback.js
+++ netlify/functions/dropbox-callback.js
@@ -1,8 +1,27 @@
+const fetch = require('node-fetch');
+const btoa = require('btoa');
+
 exports.handler = async (event, context) => {
   const code = event.queryStringParameters.code;
+  const clientSecret = process.env.DROPBOX_CLIENT_SECRET;
+
+  const response = await fetch("https://api.dropboxapi.com/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Authorization": "Basic " + btoa("ox2nxs1balk4l4i:" + clientSecret),
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      code,
+      grant_type: "authorization_code",
+      redirect_uri: "https://pdfjpgtool.com/auth/dropbox/callback"
+    })
+  });
+
+  const data = await response.json();
 
   return {
     statusCode: 200,
-    body: `Dropbox 인증이 완료되었습니다. 전달받은 코드: ${code}`
+    body: JSON.stringify(data, null, 2)
   };
 };
