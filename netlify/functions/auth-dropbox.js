a638697bc9cd08e3c0b5eff69e9e9827acadd6be
Hoddoc <149748563+Hoddoc@users.noreply.github.com>
2025-06-20 14:03:58 +0900

    Update auth-dropbox.js

--- netlify/functions/auth-dropbox.js
+++ netlify/functions/auth-dropbox.js
@@ -1,21 +1,13 @@
-// netlify/functions/auth-dropbox.js
-const querystring = require("querystring");
-
-exports.handler = async (event) => {
-  const DROPBOX_CLIENT_ID = process.env.DROPBOX_CLIENT_ID;
+exports.handler = async (event, context) => {
+  const DROPBOX_CLIENT_ID = "ox2nxs1balk4l4i";
   const REDIRECT_URI = "https://pdfjpgtool.com/auth/dropbox/callback";
 
-  const params = querystring.stringify({
-    response_type: "code",
-    client_id: DROPBOX_CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
-    token_access_type: "offline"
-  });
+  const dropboxAuthURL = `https://www.dropbox.com/oauth2/authorize?response_type=code&client_id=${DROPBOX_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
 
   return {
     statusCode: 302,
     headers: {
-      Location: `https://www.dropbox.com/oauth2/authorize?${params}`,
+      Location: dropboxAuthURL,
     },
   };
 };
